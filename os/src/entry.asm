dd54f584421c428b6b5e79d24d666966a6dcda1e
ning_guanyi <ningguanyi@haima.me>
2023-01-02 21:47:22 +0800

    添加rustsbi-qemu 0.1.1 ，支持qemu 7.2.0

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -5,8 +5,8 @@ _start:
     call rust_main
 
     .section .bss.stack
-    .globl boot_stack_lower_bound
-boot_stack_lower_bound:  #我们尝试将其放置到全局数据 .data 段中但最后未能成功，因此才决定将其放置到 .bss 段中。
+    .globl boot_stack
+boot_stack:  #我们尝试将其放置到全局数据 .data 段中但最后未能成功，因此才决定将其放置到 .bss 段中。
     .space 4096 * 16
     .globl boot_stack_top
 boot_stack_top:              # 栈顶，

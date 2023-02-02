78b2483d1fc458f517758cb3d7b7a75f3ff2d9cb
ning_hongdao <taoistwar@163.com>
2023-02-02 09:42:38 +0800

    栈溢出问题

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -14,5 +14,7 @@ _start:                         # 根据 linker.ld，我们知道 _start的地�
 # 在 linker.ld 中把 “.bss.stack” 設置為bss的第一塊指令塊。
 .section .bss.stack
 boot_stack:                     # 注：我们尝试将其放置到全局数据 .data 段中但最后未能成功，因此才决定将其放置到 .bss 段中。
-    .space 4096 * 16            # 申請空間，也就是棧空間。語法.space size [,fill]；省略吧fill，使用0值填充，功能類似於.skip。
+    # 申請空間，也就是棧空間。語法.space size [,fill]；省略吧fill，使用0值填充，功能類似於.skip。
+    # 如果栈的使用超过 64k 后，会出现什么问题？覆盖了.data、.rddata、.text段，最终修改了程序的代码。
+    .space 4096 * 16
 boot_stack_top:                 # 栈顶，

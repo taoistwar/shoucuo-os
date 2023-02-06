aabde92371ecb0b342ca6c14c53df1971a954f47
ning_hongdao <taoistwar@163.com>
2023-02-06 16:58:17 +0800

    不能正确退出

--- user/build.py
+++ user/build.py
@@ -11,17 +11,15 @@ for app in apps:
     app = app[:app.find('.')]
     lines = []
     lines_before = []
-    with open(linker, 'r', encoding="utf-8") as f:
+    with open(linker, 'r') as f:
         for line in f.readlines():
             lines_before.append(line)
-            line = line.replace(hex(base_address),
-                                hex(base_address+step*app_id))
+            line = line.replace(hex(base_address), hex(base_address+step*app_id))
             lines.append(line)
-    with open(linker, 'w+', encoding="utf-8") as f:
+    with open(linker, 'w+') as f:
         f.writelines(lines)
     os.system('cargo build --bin %s --release' % app)
-    print('[build.py] application %s start with address %s' %
-          (app, hex(base_address+step*app_id)))
-    with open(linker, 'w+', encoding='utf-8') as f:
+    print('[build.py] application %s start with address %s' %(app, hex(base_address+step*app_id)))
+    with open(linker, 'w+') as f:
         f.writelines(lines_before)
     app_id = app_id + 1
--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -30,6 +30,7 @@ pub fn sys_exit(exit_code: i32) -> isize {
 pub fn sys_yield() -> isize {
     syscall(SYSCALL_YIELD, [0, 0, 0])
 }
+
 pub fn sys_get_time() -> isize {
     syscall(SYSCALL_GET_TIME, [0, 0, 0])
 }
